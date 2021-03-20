48eb1239828d708525f9ebc46b743646fd63a7d2
ARDZ <ardz@example.com>
2021-03-20 10:41:24 +0100

    Pierwszy

---
+++ src/main/java/com/ardz/ankieter/AnkieterApplication.java
@@ -0,0 +1,13 @@
+package com.ardz.ankieter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnkieterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnkieterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ardz/ankieter/AnkieterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ardz.ankieter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnkieterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
