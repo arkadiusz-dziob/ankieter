1b723d02afc0d24929e9a26c85cd122b78f0c71a
ARDZ <ardz@demant.com>
2021-05-30 16:56:51 +0200

    Z5

---
+++ src/main/java/com/ardz/ankieter/model/Logowania.java
@@ -0,0 +1,23 @@
+package com.ardz.ankieter.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "LOGOWANIA")
+public class Logowania {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id; 
+	
+	@ManyToOne
+	private Osoba osoba;
+	
+	@ManyToOne
+	private OsobaRola osobaRola;
+}
--- src/main/java/com/ardz/ankieter/model/OsobaRola.java
+++ src/main/java/com/ardz/ankieter/model/OsobaRola.java
@@ -1,16 +1,11 @@
 package com.ardz.ankieter.model;
 
-import java.time.LocalDate;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToOne;
 import javax.persistence.Table;
-
-import org.apache.tomcat.util.security.MD5Encoder;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -18,25 +13,13 @@ import lombok.NoArgsConstructor;
 @Entity
 @Data  
 @NoArgsConstructor 
-@Table(name = "OSOBY")
-public class Osoba {
+@Table(name = "OSOBAROLA")
+public class OsobaRola {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id; 
 	
-	@Column(name = "DATA")
-	private LocalDate data;
-	
-	@Column(name = "NAZWA", length = 45)
+	@Column(name = "NAZWA", length = 45, unique = true)
 	private String nazwa;
 	
-	@Column(name = "HASLO")
-	private String haslo;
-	
-	@ManyToOne
-	private OsobaRola osobaRola;
-	
-	public void setHaslo(String haslo) {
-		this.haslo = MD5Encoder.encode(haslo.getBytes());
-	}
 }
